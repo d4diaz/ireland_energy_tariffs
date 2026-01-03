86e0306e2f4e4b9cfe5fcc2855e375437c33dcd1
Diaz Xavier <d4diaz@gmail.com>
2026-01-03 16:36:34 +0000

    Full CHange

--- custom_components/ireland_time_tariff_energy/const.py
+++ custom_components/ireland_time_tariff_energy/const.py
@@ -1,5 +1,4 @@
-DOMAIN = "ireland_energy_tariffs"
-
+DOMAIN = "ireland_time_tariff_energy"
 SENSOR_CURRENT_IMPORT = "current_import_price"
 SENSOR_CURRENT_EXPORT = "current_export_price"
 SENSOR_ACTIVE_TARIFF = "active_tariff"
